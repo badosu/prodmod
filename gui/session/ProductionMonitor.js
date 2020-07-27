beff3ad0e050b69874679f259f137884e2dc5887
Amadeus Folego <amadeusfolego@gmail.com>
2020-07-27 20:56:20 -0300

    asdklj

--- gui/session/ProductionMonitor.js
+++ gui/session/ProductionMonitor.js
@@ -146,9 +146,36 @@ ProductionMonitor.prototype.Modes = {
       }
 
       const playerQuery = this.singlePlayer() ? this.players[0] : -1;
-      const entityStates = Engine.GuiInterfaceCall("prodmod_GetPlayersProduction", playerQuery);
-      for (let entityState of entityStates.map(e => e.state)) {
-        queues[entityState.player].push(entityState);
+
+      const entityStates = Engine.GuiInterfaceCall("prodmod_GetPlayersProduction", playerQuery).map(e => e.state);
+      let foundationCounts = {};
+
+      for (let entityState of entityStates) {
+        // Push non foundation entities to queue
+        if (!entityState.foundation) {
+          queues[entityState.player].push(entityState);
+
+          continue;
+        }
+
+        // Group buildings into single item
+        const key = entityState.player.toString() + entityState.template.name;
+        const entry = foundationCounts[key];
+
+        if (!entry) {
+          foundationCounts[key] = [1, entityState];
+        } else {
+          foundationCounts[key][0]++;
+          if (foundationCounts[key][1].progress < entityState.progress)
+            foundationCounts[key][1] = entityState;
+        }
+      }
+
+      for (let key in foundationCounts) {
+        const [count, foundationState] = foundationCounts[key];
+        if (count > 1)
+          foundationState.count = count;
+        queues[foundationState.player].push(foundationState);
       }
 
       return queues;
